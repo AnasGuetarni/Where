bf7d42fdabdddcb2b930a0ede29d94babcd45a0c
AnasGuetarni <anas.guetarni@gmail.com>
2017-10-02 17:02:47 +0200

    Update slides

--- src/pages/slides/slides.ts
+++ src/pages/slides/slides.ts
@@ -19,6 +19,25 @@ export class SlidesPage {
   constructor(public navCtrl: NavController) {
   }
 
+  slides = [
+{
+  title: "Welcome to the Docs!",
+  description: "The <b>Ionic Component Documentation</b> showcases a number of useful components that are included out of the box with Ionic.",
+  image: "",
+},
+{
+  title: "What is Ionic?",
+  description: "<b>Ionic Framework</b> is an open source SDK that enables developers to build high quality mobile apps with web technologies like HTML, CSS, and JavaScript.",
+  image: "",
+},
+{
+  title: "What is Ionic Cloud?",
+  description: "The <b>Ionic Cloud</b> is a cloud platform for managing and scaling Ionic apps with integrated services like push notifications, native builds, user auth, and live updating.",
+  image: "",
+}
+];
+
+
   ionViewDidLoad() {
     console.log('ionViewDidLoad Slides');
   }
